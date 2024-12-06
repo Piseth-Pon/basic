2c2cbdb53b21865ed3c42952ce617a630aa0e610
Piseth-Pon <ponpiseth8@gmail.com>
2024-12-07 06:36:23 +0700

    add random background

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('everything is ready')
+
+function randomBackground() {
+  const body = document.querySelector("body")
+  body.style.backgroundColor = `rgba(${Math.random() * 255, Math.random() * 255, Math.random() * 255, 1})`
+}
+
+
+
+window.addEventListener("DOMContentLoaded", () => {
+  console.log("everything is ready")
+  randomBackground()
+})
